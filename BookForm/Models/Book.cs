dfe254985ee17819fdfee453b987ee269b492e18
Tina Greene <46331543+greeneteenee@users.noreply.github.com>
2020-01-18 18:05:44 -0800

    Created book class

---
+++ BookForm/Models/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookForm.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+
+        public string BookTitle { get; set; }
+
+        public string Author { get; set; }
+
+        public string Isbn { get; set; }
+
+        public DateTime PublicationDate { get; set; }
+
+
+    }
+}

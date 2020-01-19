c8203d99928f812b5b7d659ef82e15c4e9a6d9aa
Tina Greene <46331543+greeneteenee@users.noreply.github.com>
2020-01-18 17:51:50 -0800

    Add project files.

---
+++ BookForm/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BookForm.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

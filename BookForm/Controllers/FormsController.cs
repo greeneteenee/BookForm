ffbea37f34085cc3acb584caae13564cb57d1374
Tina Greene <46331543+greeneteenee@users.noreply.github.com>
2020-01-18 19:43:54 -0800

    Mapped form data to C# book object

--- BookForm/Controllers/FormsController.cs
+++ BookForm/Controllers/FormsController.cs
@@ -2,14 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookForm.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookForm.Controllers
 {
     public class FormsController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(IFormCollection formData)
+        {
+            //Map form data to Book Object
+            Book book = new Book();
+            book.BookTitle = formData["book-title"];
+            book.Author = formData["author"];
+            book.Isbn = formData["isbn"];
+            book.PublicationDate = Convert.ToDateTime(formData["publication-date"]);
+
+            //Add to database
+
+            ViewData["Message"] = "Book added successfully";
+
             return View();
         }
     }
